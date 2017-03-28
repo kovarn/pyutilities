6a73b70d9987fc48750c85c9adeccad7c51351d3
mustafa <kovarn@users.noreply.github.com>
2017-03-28 12:51:01 +0530

    Simplify code for async_progress
    Handle tqdm instance directly instead of going through an extra generator.

--- utils/multiprocessing_.py
+++ utils/multiprocessing_.py
@@ -1,20 +1,18 @@
+from tqdm import tqdm
+from multiprocessing import pool
+
+
 # noinspection PyProtectedMember
-def async_progress(res, desc=None, wait_time=0.5):
-    import time
-    from tqdm import tqdm
-    from .itertools_ import consume
-
+def async_progress(res: pool.MapResult, desc: str = None, wait_time: float = 0.5):
     total = res._number_left
+    tq = tqdm(desc=desc, total=total)
 
-    # noinspection PyProtectedMember
-    def progress():
+    prev = res._number_left
+    while not res.ready():
+        if res._number_left < prev:
+            tq.update(prev - res._number_left)
         prev = res._number_left
-        while True:
-            while res._number_left == prev and not res.ready():
-                time.sleep(0.5)
-            prev -= 1
-            yield prev
-            if prev <= 0:
-                break
-
-    consume(tqdm(progress(), total=total, desc=desc), None)
+        # time.sleep(wait_time)
+        res.wait(wait_time)
+    tq.update(prev - res._number_left)
+    tq.close()
